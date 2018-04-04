b1347959ba4e1929c6233cf9ea39358229dc12b4
ojoifeanyi <37763914+ojoifeanyi@users.noreply.github.com>
2018-04-03 19:17:34 -0700

    initial upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!-- saved from url=(0027)http://apply.felixihub.com/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ifeanyi Ojo - FavCode Test</title>
+
+	<meta name="description" content="African development through partnership">
+
+
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Submission for Felix - Favcode ">
+	<meta property="og:type" content="article">
+	<meta property="og:image" content="ifeanyi.jpg">
+	<meta property="og:description" content="African development through partnership">
+
+	<link rel="shortcut icon"
+ href="fav.jpg">
+
+	<link rel="stylesheet" href="ifeanyi_ojo.css">
+</head>
+<body cz-shortcut-listen="true">
+	<div class="container">
+
+		<div class="topnav" id="myTopnav">
+  <a href="#"></a>
+  <a href="#">Projects</a>
+  <a href="#">Contact me</a>
+  <a href="#profile">Profile</a>
+  <a href="#">Home</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+
+
+
+		<div class="myheader">
+			<p class="left">Hi, I'm Ifeanyi Ojo, a #FavCoder.</p>
+				<p class="right">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Nuv_ncYajWg?rel=0" frameborder="0"  encrypted-media" allowfullscreen></iframe>
+			</p>
+		</div>
+
+		<div class="mission" style="color:#424949; background-color:#ECF0F1; font-size:23px;text-align:center;height:auto;padding: 80px;font-family:Consolas;">
+			<blockquote>
+				<p style="color:black;">MISSION</p><br>
+				<p  style="font-family:Calibri;">My mission for Favcode is to:  Develop and deploy enterprise software products, hence using Information system to solve todays problems.</p>
+
+			</blockquote>
+				
+		</div>
+		<div class="me" style="color:#979A9A; background-color:#212627  ; font-size:16px;text-align:center;height:auto;padding: 80px;font-family:Consolas;">
+			<blockquote>
+				 <p style="color:white;font-size: 28px;">This is why you should choose me</p><br>
+                <p  style="font-family:Calibri;" >I may not be the best, however 3 things makes me different viz:</p><br>
+                <p  style="font-family:Calibri;"><b >Passion</b> - This is something that keeps me going against all odds. </p><br>
+                 <p  style="font-family:Calibri;"><b >Ability To Learn New Stuff And At A Fast Pace</b> - Almost every year, new technology comes into light and it is always required that one most keep updating oneself on the use of these new techs. I can learn new things at a fast pace once i am commited to it .  </p><br>
+                 <p  style="font-family:Calibri;"><b >Team Work</b> - Been able to work independenty and as a Team can bring about better and faster results, these i am capable</p><br>
+
+                 <p  style="font-family:Calibri;">When i am given the right skills, opportunity and a platform, i will be able to achieve better and faster results.</p><br>
+
+            
+			</blockquote>
+				
+		</div>
+		<div id="profile" class="imgg" style="color:#424949;font-size:23px;text-align:center;height:auto;padding: 50px;font-family:calibri;">
+			<blockquote>
+				<img src="ifeanyi.jpg" alt="">
+
+				<br><br><p style="color:#424949;text-align: center;font-weight: bold;font-family:Consolas;">PROFILE</p><br>
+							<p style="color:#424949;"><strong>Name:</strong> Ifeanyichukwu Ike Ojo</p>
+							<p style="color:#424949;"><strong>Location:</strong> Port Harcourt, Nigeria.</p>
+							<p style="color:#424949;"><strong>Passion:</strong> Programming, Information System, Tech Enthusiast</p>
+							<p style="color:#424949;"><strong>Skills:</strong> Chat bot Developer, Back-end Developer</p>
+							<p style="color:#424949;"><strong>Status:</strong> Student at <a href="https://favcode54.org/" target="_blank" style="text-decoration:none;">#FavCode54</a></p>
+							 <p class="meta">Here is my <a href="https://www.linkedin.com/in/ifeanyiojo" style="text-decoration: none" target="_blank">LinkedIn Page</a>.</p>
+			</blockquote>
+				
+		</div>
+
+		<footer class="foot">
+			<p>March 28, 2018. Submission for Felix - Favcode Partnership</p>
+			<p>Made by <a href="https://facebook.com/ifeanyi.ojo.12" style="color:white;" target="_blank">Ifeanyi Ojo.</a></p>
+		</footer>
+	</div>
+
+
+
+
+
+<script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+</body>
+
+</html>
